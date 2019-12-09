b3910e5cd9c89b8b7853be07d347f46aa0639826
jnowak90 <30462355+jnowak90@users.noreply.github.com>
2019-12-09 14:56:27 +0100

    Clean script

--- ExtractionPipeline.py
+++ ExtractionPipeline.py
@@ -16,15 +16,11 @@ import pandas as pd
 import random
 import scipy as sp
 import skimage
-import skimage.io
-import skimage.filters
+from skimage import io, filters
 import sys
 import glob
 from collections import Counter
 import utils
-
-print("\nYour python version used for this script:")
-print(sys.version)
 
 class CytoSeg:
 
@@ -51,27 +47,19 @@ class CytoSeg:
         self.filterImg = glob.glob('*_filter.tif')
 
         self.imgRaw = skimage.io.imread(self.filterImg[0], plugin='tifffile')
-        if self.imgRaw.shape[2] in (3,4):
+        if self.imgRaw.shape[2] in (3, 4):
             self.imgRaw = np.swapaxes(self.imgRaw, -1, -3)
             self.imgRaw = np.swapaxes(self.imgRaw, -1, -2)
         self.slices = len(self.imgRaw)
-        """
-        add analysis for 3D graphs
-        self.Z = 1
-        self.shape = self.imgRaw.shape
-        if (len(self.shape) > 3):
-            self.Z = self.shape[3]
-        """
 
         # find and open image mask
         self.maskImg = glob.glob('*_mask.tif')
         self.mask = skimage.io.imread(self.maskImg[0], plugin='tifffile') > 0
 
-        print('Start extraction of image',self.filterImg[0])
+        print('Start extraction of image', self.filterImg[0])
         for i in range(self.slices):
             print('\n' , i+1, 'of', self.slices)
             self.imgSlice = self.imgRaw[i]
-            self.imgSlice = utils.im2d3d(self.imgSlice)
             self.imgGaussian = skimage.filters.gaussian(self.imgSlice, self.sigma)
             self.imgTube, self.imgSkeleton = utils.skeletonize_graph(self.imgGaussian, self.mask, self.sigma, self.block, self.small, self.factr)
             self.imgNodes = utils.node_graph(self.imgSkeleton > 0, self.imgGaussian)
@@ -81,7 +69,7 @@ class CytoSeg:
             self.originalData.append([i, self.originalNormalizedGraph, self.originalPosition, self.originalProperties])
 
             for r in range(self.randn):
-                self.randomGraph, self.randomPosition = utils.randomize_graph(self.unifiedGraph, self.originalPosition, self.mask, planar=1, weights=self.randw)
+                self.randomGraph, self.randomPosition = utils.randomize_graph(self.unifiedGraph, self.originalPosition, self.mask)
                 self.randomNormalizedGraph, self.randomProperties, _ = self.processGraph(self.randomGraph, self.randomPosition, self.imgGaussian, self.mask)
                 self.randomData.append([i, self.randomNormalizedGraph, self.randomPosition, self.randomProperties])
 
@@ -99,26 +87,26 @@ class CytoSeg:
         self.normalizedGraph = utils.normalize_graph(self.centralizedGraph)
         self.graphProperties = utils.compute_graph(self.normalizedGraph, graphPosition, mask)
         return(self.normalizedGraph, self.graphProperties, self.unifiedGraph)
-        
-    def most_frequent(self, List): 
-        occurence_count = Counter(List) 
-        return occurence_count.most_common(1)[0][0] 
+
+    def most_frequent(self, List):
+        occurence_count = Counter(List)
+        return occurence_count.most_common(1)[0][0]
 
     def plotSkeleton(self, originalData, randomData):
             originalGraph, originalPosition = originalData[0][1], originalData[0][2]
             randomGraph, randomPosition = randomData[0][1], randomData[0][2]
-          
+
             if self.most_frequent(self.imgRaw[0].flatten()) <= 128:
                 cmapImage = 'gray_r'
             else:
                 cmapImage = 'gray'
-                
+
             fig, (ax1, ax2) = plt.subplots(ncols=2)
-            
+
             ax1.imshow(self.imgRaw[0], cmap=cmapImage)
             ax1.set_title('Biological actin network', fontsize=7)
-            originalEdgeCapacity = 1.0 * np.array([d['capa'] for u,v,d in originalGraph.edges(data=True)])
-            nx.draw_networkx_edges(originalGraph, originalPosition[:,:2], edge_color=plt.cm.plasma(originalEdgeCapacity / originalEdgeCapacity.max()), width=1.2, ax=ax1)
+            originalEdgeCapacity = 1.0 * np.array([property['capa'] for node1, node2, property in originalGraph.edges(data=True)])
+            nx.draw_networkx_edges(originalGraph, originalPosition, edge_color=plt.cm.plasma(originalEdgeCapacity / originalEdgeCapacity.max()), width=1.2, ax=ax1)
             divider = make_axes_locatable(ax1)
             cax1 = divider.append_axes("right", size="5%", pad=0.05)
             m1 = plt.cm.ScalarMappable(cmap="plasma")
@@ -127,11 +115,11 @@ class CytoSeg:
             cbar1.ax.tick_params(labelsize=7)
             ax1.axes.get_yaxis().set_visible(False)
             ax1.axes.get_xaxis().set_visible(False)
-            
+
             ax2.imshow(self.imgRaw[0], cmap=cmapImage)
             ax2.set_title('Randomized actin network', fontsize=7)
-            randomEdgeCapacity = 1.0 * np.array([d['capa'] for u,v,d in randomGraph.edges(data=True)])
-            nx.draw_networkx_edges(randomGraph, randomPosition[:,:2], edge_color=plt.cm.plasma(randomEdgeCapacity / randomEdgeCapacity.max()), width=1.2, ax=ax2)
+            randomEdgeCapacity = 1.0 * np.array([property['capa'] for node1, node2, property in randomGraph.edges(data=True)])
+            nx.draw_networkx_edges(randomGraph, randomPosition, edge_color=plt.cm.plasma(randomEdgeCapacity / randomEdgeCapacity.max()), width=1.2, ax=ax2)
             divider = make_axes_locatable(ax2)
             cax2 = divider.append_axes("right", size="5%", pad=0.05)
             m2 = plt.cm.ScalarMappable(cmap="plasma")
@@ -140,20 +128,20 @@ class CytoSeg:
             cbar2.ax.tick_params(labelsize=7)
             ax2.axes.get_yaxis().set_visible(False)
             ax2.axes.get_xaxis().set_visible(False)
-            
+
             plt.tight_layout(h_pad=1)
-            
+
             if self.osSystem == 1:
                 fig.savefig(self.pathToFolder + "\\ExtractedNetworks.png", box_inches="tight", dpi=300)
             else:
                 fig.savefig(self.pathToFolder + "/ExtractedNetworks.png", box_inches="tight", dpi=300)
 
     def saveData(self, originalData, randomData):
-        properties = ['time','# nodes','# edges','# connected components','avg. edge capacity','assortativity','avg. path length','CV path length','algebraic connectivity','CV edge angles','crossing number']
+        properties = ['time', '# nodes', '# edges', '# connected components', 'avg. edge capacity', 'assortativity', 'avg. path length', 'CV path length', 'algebraic connectivity', 'CV edge angles', 'crossing number']
 
         # original graphs
         originalPositions = np.array([np.hstack([d[2]]) for d in originalData])
-        originalProperties = np.array([np.hstack([d[0],d[-1]]) for d in originalData])
+        originalProperties = np.array([np.hstack([d[0], d[-1]]) for d in originalData])
         originalGraphs = [d[1] for d in originalData]
         df = pd.DataFrame(originalProperties, columns=properties)
         if self.osSystem == 1:
@@ -167,7 +155,7 @@ class CytoSeg:
 
         # random graphs
         randomPositions = np.array([np.hstack([d[2]]) for d in randomData])
-        randomProperties = np.array([np.hstack([d[0],d[-1]]) for d in randomData])
+        randomProperties = np.array([np.hstack([d[0], d[-1]]) for d in randomData])
         randomGraphs = [d[1] for d in randomData]
         df = pd.DataFrame(randomProperties, columns=properties)
         if self.osSystem == 1:
