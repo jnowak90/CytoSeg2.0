62d2a66df7d88e9c9e912f1c24458a3cc88d921a
jnowak90 <30462355+jnowak90@users.noreply.github.com>
2019-12-20 10:02:53 +0100

    Add check for Python 2 vs. 3

--- SystemTest.py
+++ SystemTest.py
@@ -13,20 +13,23 @@ import sys
 print("\nYour python version used for this script:")
 print(sys.version)
 
-path = sys.argv[1]
-f = open(path, "w")
-f.write("This is a file to test if the right Python 3 path was found.")
-f.close()
+if sys.version_info >= (3, 0):
+    path = sys.argv[1]
+    f = open(path, "w")
+    f.write("This is a file to test if the right Python 3 path was found.")
+    f.close()
 
-modules = ['matplotlib', 'networkx', 'numpy', 'pandas', 'PIL', 'scipy', 'skimage', 'shapely', 'packaging', 'collections', 'glob', 'itertools', 'tkinter']
+    modules = ['matplotlib', 'networkx', 'numpy', 'pandas', 'PIL', 'scipy', 'skimage', 'shapely', 'packaging', 'collections', 'glob', 'itertools', 'tkinter']
 
-counter = 0
-for module in modules:
-    try:
-        importlib.import_module(module)
-    except ImportError:
-        print('Python module: ' + '"' + module + '"' + ' not found. Please install before continuing.')
-        counter += 1
+    counter = 0
+    for module in modules:
+        try:
+            importlib.import_module(module)
+        except ImportError:
+            print('Python module: ' + '"' + module + '"' + ' not found. Please install before continuing.')
+            counter += 1
 
-if counter == 0:
-    print("\n### All necessary Python modules were found.")
+    if counter == 0:
+        print("\n### All necessary Python modules were found.")
+else:
+    print("\nError: you are using Python 2, but a path to Python 3 is required.")
